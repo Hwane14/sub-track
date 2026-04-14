54f78ae297f552bb25ce594cb287787604d11463
Hamidou Wane <wanen717@gmail.com>
2026-04-14 19:12:15 +0100

    Add React Router and wrap App in BrowserRouter for navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+
 function App() {
   return (
-    <div>
-      <h1>Subscription Tracker</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
   );
 }
 
